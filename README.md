bf60c37515bab17d90675850d69bd8e72ea66bdb
Nalla Senthilnathan <nrsenthil@gmail.com>
2025-01-06 11:56:45 -0500

    first iter

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# gin-crud-apis
-gin-crud-apis
+## gin-crud-apis README
+This project demos serving golang/gin based REST CRUD APIs. 
+
+The code follows the tutorial at: https://www.allhandsontech.com/programming/golang/web-app-sqlite-go/
+
+This project serves as a backend for https://github.com/ns-code/ng18-maintainusers-refactored
+
+It uses an embedded SQLLite DB. The follwing APIs are currently supported:
+
+GET /api/users
+
+### Usage Notes
+After cloning this repo, download and install https://jmeubank.github.io/tdm-gcc/download/ and set the PATH
+
+Then run<br>
+go env -w CGO_ENABLED=1
+
+Then run<br>
+go run .
---
+++ models/user.go
@@ -0,0 +1,166 @@
+package models
+
+import (
+	"database/sql"
+	"fmt"
+	"strconv"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var USERDB *sql.DB
+
+func ConnectUserDatabase() error {
+	db, err := sql.Open("sqlite3", "./users.db")
+	if err != nil {
+		return err
+	}
+
+	USERDB = db
+	return nil
+}
+
+type User struct {
+	UserId        int `json:"userId"`
+	UserName   string `json:"userName"`
+	FirstName  string `json:"firstName"`
+	LastName   string `json:"lastName"`
+	Email      string `json:"email"`
+	UserStatus string `json:"userStatus"`
+	Department string `json:"department"`
+}
+
+func GetUsers(count int) ([]User, error) {
+
+	rows, err := USERDB.Query("SELECT user_id, user_name, first_name, last_name, email, user_status, department from user LIMIT " + strconv.Itoa(count))
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	users := make([]User, 0, count)
+
+	for rows.Next() {
+		singleUser := User{}
+		err = rows.Scan(&singleUser.UserId, &singleUser.UserName, &singleUser.FirstName, &singleUser.LastName, &singleUser.Email, &singleUser.UserStatus, &singleUser.Department)
+
+		if err != nil {
+			return nil, err
+		}
+
+		users = append(users, singleUser)
+		fmt.Println(">> Users count: ", len(users))
+	}
+
+	err = rows.Err()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return users, err
+}
+
+/*
+func GetUserById(id string) (User, error) {
+
+	stmt, err := DB.Prepare("SELECT id, first_name, last_name, email, ip_address from people WHERE id = ?")
+
+	if err != nil {
+		return User{}, err
+	}
+
+	person := User{}
+
+	sqlErr := stmt.QueryRow(id).Scan(&person.Id, &person.FirstName, &person.LastName, &person.Email, &person.IpAddress)
+
+	if sqlErr != nil {
+		if sqlErr == sql.ErrNoRows {
+			return User{}, nil
+		}
+		return User{}, sqlErr
+	}
+	return person, nil
+}
+
+func AddUser(newUser User) (bool, error) {
+
+	tx, err := DB.Begin()
+	if err != nil {
+		return false, err
+	}
+
+	stmt, err := tx.Prepare("INSERT INTO people (first_name, last_name, email, ip_address) VALUES (?, ?, ?, ?)")
+
+	if err != nil {
+		return false, err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(newUser.FirstName, newUser.LastName, newUser.Email, newUser.IpAddress)
+
+	if err != nil {
+		return false, err
+	}
+
+	tx.Commit()
+
+	return true, nil
+}
+
+func UpdateUser(ourUser User, id int) (bool, error) {
+
+	tx, err := DB.Begin()
+	if err != nil {
+		return false, err
+	}
+
+	stmt, err := tx.Prepare("UPDATE people SET first_name = ?, last_name = ?, email = ?, ip_address = ? WHERE Id = ?")
+
+	if err != nil {
+		return false, err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(ourUser.FirstName, ourUser.LastName, ourUser.Email, ourUser.IpAddress, id)
+
+	if err != nil {
+		return false, err
+	}
+
+	tx.Commit()
+
+	return true, nil
+}
+
+func DeleteUser(personId int) (bool, error) {
+
+	tx, err := DB.Begin()
+
+	if err != nil {
+		return false, err
+	}
+
+	stmt, err := DB.Prepare("DELETE from people where id = ?")
+
+	if err != nil {
+		return false, err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(personId)
+
+	if err != nil {
+		return false, err
+	}
+
+	tx.Commit()
+
+	return true, nil
+}
+*/
